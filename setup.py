31b370ee1ba776587c3c17b498c3726f106ed2f8
Jordan Leppert <jordanleppert@gmail.com>
2021-10-20 16:49:57 +0100

    Version 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="btrfssnapshottools",
-    version="1.0.1",
+    version="1.1.0",
     author="Jordan Leppert",
     author_email="jordanleppert@gmail.com",
     description="A tool to take regular snapshots of BTRFS subvolumes, and optionally back them up to a remote location.",
